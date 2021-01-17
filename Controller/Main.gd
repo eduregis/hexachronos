3fbce25e96c66109e6d89640c2873d6fc8a4e8c1
Eduardo <eduregis1@gmail.com>
2021-01-16 23:28:49 -0300

    tile emitting signal by click

--- Controller/Main.gd
+++ Controller/Main.gd
@@ -3,20 +3,24 @@ extends Node2D
 var DialogueBox = preload("res://View/DialogueBox.tscn")
 var Tile = preload("res://View/Components/HexagonTile.tscn")
 
-var tile_map = [
-	[0, 1, 1, 1],
-	[1, 1, 1, 1],
-	[1, 1, 1, 1, 1],
-	[1, 1, 1, 1],
-	[0, 1, 1, 1],
-]
+var tile_map = []
 
 var tile_map_position = []
 
 func _ready():
-	instance_tilemap()
+	load_tilemap()
 	#load_dialogue("0001")
 	
+func load_tilemap():
+	tile_map = [
+		[0, 1, 1, 1],
+		[1, 1, 1, 1],
+		[1, 1, 1, 1, 1],
+		[1, 1, 1, 1],
+		[0, 1, 1, 1],
+	]
+	instance_tilemap()
+
 func instance_tilemap():
 	var window = get_viewport_rect().size
 	var x = 0
@@ -28,6 +32,7 @@ func instance_tilemap():
 			var tile_position = Vector2(window.x/6 + x*185 + (y % 2)*92, window.y/2 + y *30)
 			var tile = Tile.instance()
 			tile.set_position(tile_position)
+			tile.tile_index = Vector2(x, y)
 			tile_vector_position.append(tile)
 			self.add_child(tile)
 			x += 1
