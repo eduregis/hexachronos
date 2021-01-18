690f758da25e311821164812bb1b5b5b047f2ed2
Eduardo <eduregis1@gmail.com>
2021-01-18 03:17:57 -0300

    parable animation to movement

--- Controller/Components/Character.gd
+++ Controller/Components/Character.gd
@@ -5,9 +5,23 @@ export var character_info = {}
 export var team = ""
 
 func move_to(tile_position):
-	$Tween.interpolate_property(
-			self, "position", position , tile_position, 1, 
-			Tween.TRANS_SINE, Tween.EASE_IN_OUT
+	var tween1 = get_node("Tween")
+	var tween2 = get_node("Tween")
+	tween1.interpolate_property(
+			self, "position:x", position.x , tile_position.x, 0.6, 
+			Tween.TRANS_LINEAR, Tween.TRANS_LINEAR
 		)
-	$Tween.start()
+	tween1.start()
+	var yAnchor = position.y - 30
+	tween2.interpolate_property(
+			self, "position:y", position.y , yAnchor, 0.3, 
+			Tween.TRANS_SINE, Tween.EASE_OUT
+		)
+	tween2.start()
+	yield(tween2, "tween_completed")
+	tween2.interpolate_property(
+			self, "position:y", yAnchor, tile_position.y, 0.3, 
+			Tween.TRANS_SINE, Tween.EASE_IN
+		)
+	tween2.start()
 #	position = tile_position
