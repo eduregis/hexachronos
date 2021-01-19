57ff224511a124d979327f22ea7232e79625ac0a
Eduardo <eduregis1@gmail.com>
2021-01-19 17:00:12 -0300

    background component

--- Controller/Scenes/Scene1.gd
+++ Controller/Scenes/Scene1.gd
@@ -1,10 +1,11 @@
 extends "res://Controller/Scenes/BasicScene.gd"
 
 func _ready():
+#	load_background("black")
 	load_tilemap("0001")
 	yield(get_tree().create_timer(1), "timeout")
 	set_ally("Tanker", 1, 1)
 	set_ally("Mechanic", 2, 2)
 	set_foe("Foe", 3, 2)
 #	load_dialogue("0001")
-	
+	pass
